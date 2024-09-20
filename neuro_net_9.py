2421520b1b9dc930e0bc462f773d0d7845f4d5b3
selfedu-rus <65663799+selfedu-rus@users.noreply.github.com>
2024-09-20 16:40:44 +0400

    Add files via upload

--- neuro_net_9.py
+++ neuro_net_9.py
@@ -6,14 +6,14 @@ def act(x):
 
 
 def go(house, rock, attr):
-    X = torch.FloatTensor([house, rock, attr])
-    Wh = torch.FloatTensor([[0.3, 0.3, 0], [0.4, -0.5, 1]])  # матрица 2x3
-    Wout = torch.FloatTensor([-1, 1])     # вектор 1х2
+    X = torch.tensor([house, rock, attr], dtype=torch.float32)
+    Wh = torch.tensor([[0.3, 0.3, 0], [0.4, -0.5, 1]])  # матрица 2x3
+    Wout = torch.tensor([-1.0, 1.0])     # вектор 1х2
 
     Zh = torch.mv(Wh, X)       # вычисляем сумму на входах нейронов скрытого слоя
     print(f"Значения сумм на нейронах скрытого слоя: {Zh}")
 
-    Uh = torch.FloatTensor([act(x) for x in Zh])
+    Uh = torch.tensor([act(x) for x in Zh], dtype=torch.float32)
     print(f"Значения на выходах нейронов скрытого слоя: {Uh}")
 
     Zout = torch.dot(Wout, Uh)
--- neuro_net_9gpu.py
+++ neuro_net_9gpu.py
@@ -2,20 +2,19 @@ import torch
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-
 def act(x):
     return 0 if x < 0.5 else 1
 
 
 def go(house, rock, attr):
-    X = torch.FloatTensor([house, rock, attr]).to(device)
-    Wh = torch.FloatTensor([[0.3, 0.3, 0], [0.4, -0.5, 1]]).to(device)  # матрица 2x3
-    Wout = torch.FloatTensor([-1, 1]).to(device)     # вектор 1х2
+    X = torch.tensor([house, rock, attr], dtype=torch.float32, device=device)
+    Wh = torch.tensor([[0.3, 0.3, 0], [0.4, -0.5, 1]], device=device)  # матрица 2x3
+    Wout = torch.tensor([-1.0, 1.0], device=device)     # вектор 1х2
 
     Zh = torch.mv(Wh, X)       # вычисляем сумму на входах нейронов скрытого слоя
     print(f"Значения сумм на нейронах скрытого слоя: {Zh}")
 
-    Uh = torch.FloatTensor([act(x) for x in Zh]).to(device)
+    Uh = torch.tensor([act(x) for x in Zh], dtype=torch.float32, device=device)
     print(f"Значения на выходах нейронов скрытого слоя: {Uh}")
 
     Zout = torch.dot(Wout, Uh)
